313e9633f2518273e8d01296c479f10549b85016
idajoh <bp17idajoh@gmail.com>
2024-04-03 16:30:41 +0200

    new angular setup

---
+++ recipe-app-ida2/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponen
+import { RecipeAPIComponent } from './recipe/recipe.component';
+import { RecipeAPIDetailComponent } from './api-detail/apidetail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+
+
+const routes: Routes = [
+  { path: '', component: RecipeAPIComponent },
+  { path: 'recipe/:id', component: RecipeAPIDetailComponent },
+  { path: 'login', component: LoginComponent  },
+  { path: 'register', component: SignupComponent  },
+  { path: '**', component: NotFoundComponent, data: { title : 'Oops' }  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
